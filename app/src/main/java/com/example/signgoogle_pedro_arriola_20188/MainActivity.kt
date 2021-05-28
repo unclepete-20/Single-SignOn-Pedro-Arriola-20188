48db5810508cbd85f1b56a415beadf40cca0c66e
unclepete-20 <pedroparty310@gmail.com>
2021-05-28 14:10:20 -0600

    Se integro Firebase para implementar el Sign On con Google.

---
+++ app/src/main/java/com/example/signgoogle_pedro_arriola_20188/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.signgoogle_pedro_arriola_20188
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        Handler().postDelayed({
+
+        })
+    }
+}
