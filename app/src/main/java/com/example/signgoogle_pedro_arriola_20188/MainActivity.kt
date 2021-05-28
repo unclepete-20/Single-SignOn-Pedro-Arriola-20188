548b7b0c11cb897ead662072078854d0a0154ec8
unclepete-20 <pedroparty310@gmail.com>
2021-05-28 17:06:56 -0600

    Se implemento Google Services para inicio de sesion en la aplicacion.

--- app/src/main/java/com/example/signgoogle_pedro_arriola_20188/MainActivity.kt
+++ app/src/main/java/com/example/signgoogle_pedro_arriola_20188/MainActivity.kt
@@ -1,16 +1,34 @@
 package com.example.signgoogle_pedro_arriola_20188
 
+import android.content.Intent
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var mAuth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        Handler().postDelayed({
+        getWindow().getDecorView().setBackgroundColor(Color.rgb(229,255,229));
 
-        })
+        mAuth = FirebaseAuth.getInstance()
+        val user = mAuth.currentUser
+
+        Handler().postDelayed({
+              if (user != null){
+                  val dashboardIntent = Intent(this, DashboardActivity::class.java)
+                  startActivity(dashboardIntent)
+
+              } else {
+                  val signInIntent = Intent(this,SignInActivity::class.java)
+                  startActivity(signInIntent)
+              }
+
+        }, 2000)
     }
 }
